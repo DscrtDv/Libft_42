98519858a4af603f330d042b49dc98f13b8b19b4
Timothe Censier <tcensier@f0r4s21.codam.nl>
2022-10-10 15:40:43 +0200

    Second commit: School/ added makefile

---
+++ Makefile
@@ -0,0 +1,41 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         ::::::::             #
+#    Makefile                                           :+:    :+:             #
+#                                                      +:+                     #
+#    By: tcensier <marvin@codam.nl>                   +#+                      #
+#                                                    +#+                       #
+#    Created: 2022/10/10 15:04:03 by tcensier      #+#    #+#                  #
+#    Updated: 2022/10/10 15:38:21 by tcensier      ########   odam.nl          #
+#                                                                              #
+# **************************************************************************** #
+
+NAME := libft.a
+HEADERFILES := libft.h
+SRC := ft_bzero.c ft_isalnum.c ft_isalpha.c ft_isascii.c ft_isdigit.c ft_isprint.c ft_memcpy.c ft_memmove.c ft_memset.c ft_strlen.c
+OBJFILES := $(SRC:.c=.o)
+LDFLAGS ?=
+CFLAGS ?= -Wall -Werror -Wextra
+
+all: $(NAME)
+
+$(NAME): $(OBJFILES)
+	@ar rcs $(NAME) $(OBJFILES)
+	@echo "Archive file $(NAME) successfully created and indexed."
+
+%.o: %.c $(HEADERFILES)
+	@$(CC) -c $(CFLAGS) -o $@ $<
+
+clean:
+	@rm -f $(OBJFILES)
+	@echo "OBJ files successfully deleted."
+
+fclean: clean
+	@rm -f $(NAME)
+	@echo "$(NAME) successfully deleted."
+
+re: fclean all
+
+.PHONY: all, clean, fclean, re
+
+
--- ft_memmove.c
+++ ft_memmove.c
@@ -1,24 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   ft_memcpy.c                                        :+:    :+:            */
+/*   ft_memmove.c                                       :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: tcensier <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2022/10/07 13:09:09 by tcensier      #+#    #+#                 */
-/*   Updated: 2022/10/10 12:29:23 by tcensier      ########   odam.nl         */
+/*   Created: 2022/10/10 12:28:47 by tcensier      #+#    #+#                 */
+/*   Updated: 2022/10/10 12:32:21 by tcensier      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	char		*d;
-	const char	*s;
+	char	*temp_dest;
+	char	*temp_src;
 
-	d = (char *) dest;
-	s = (const char *) src;
-	while (n--)
-		*(d++) = *(s++);
+	temp_dest = (char *) dest;
+	temp_src = (char *) src;
+	if ((temp_dest == 0) || (temp_src == 0) || n == 0)
+		return (dest);
+	if (temp_src < temp_dest && temp_dest < temp_src + n)
+	{
+		while (n--)
+			temp_dest[n] = temp_src[n];
+	}
+	else
+	{
+		while (n--)
+		{
+			*(temp_dest++) = *(temp_src++);
+		}
+	}
 	return (dest);
 }
